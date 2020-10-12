cae24b33ed016ac394e765ca599e822320537c54
ewfczc <72751050+ewfczc@users.noreply.github.com>
2020-10-12 22:49:13 +0800

    Add files via upload

---
+++ 2_5/2_5/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_5
+{
+    abstract class Calculator
+    {
+
+        public abstract int Task(int num1, int num2);
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            Calculator comObj = obj as Calculator;
+            if (comObj == null)
+            {
+                return false;
+            }
+            else
+            {
+                return base.Equals(comObj);
+            }
+        }
+    }
+}
---
+++ 2_5/2_5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("请输入整型操作数及操作符（例： 1 + 2 ）：");
+                string str = Console.ReadLine();
+                string[] data = str.Split(' ');
+
+                Calculator count;
+
+                switch (data[1])
+                {
+                    case "+": Add add = new Add(); count = add; Console.WriteLine("和是：{0}", count.Task(int.Parse(data[0]), int.Parse(data[2]))); break;
+                    case "-": Subtract sub = new Subtract(); count = sub; Console.WriteLine("差是：{0}", count.Task(int.Parse(data[0]), int.Parse(data[2]))); break;
+                    case "*": Multiply mtp = new Multiply(); count = mtp; Console.WriteLine("积是：{0}", count.Task(int.Parse(data[0]), int.Parse(data[2]))); break;
+                    case "/": Divide dvd = new Divide(); count = dvd; Console.Write("商是："); count.Task(int.Parse(data[0]), int.Parse(data[2])); break;
+                    case "&": And and = new And(); count = and; Console.WriteLine("按位与结果是：{0}", count.Task(int.Parse(data[0]), int.Parse(data[2]))); break;
+                    case "%": Remainder rmd = new Remainder(); count = rmd; Console.WriteLine("余数是：{0}", count.Task(int.Parse(data[0]), int.Parse(data[2]))); break;
+                }
+            }
+
+        }
+    }
+}
